fb216a427d0b3be411d65a21ead68bee9fd35f53
abhijaygh <42239188+abhijaygh@users.noreply.github.com>
2019-10-05 19:06:44 +0530

    UPDATED THE PROGRAM WITH COMMENTS
    I HAVE UPDATED THE CODE WITH COMMENTS AND REMOVED OUT SOME UNECESSARY STEPS. I HAVE ALSO REVISED THE INDENDATION TO ENHANCE THE READABILITY.

--- sorting/mergesort.c
+++ sorting/mergesort.c
@@ -6,42 +6,49 @@ void swap (int *a,int *b)//To swap the variables//
     t= *a;
     *a=*b;
     *b=t;
-    
 }
 
-void merge(int a[],int l,int r,int n)//To merge //
-{  int *b = (int*)malloc(n*sizeof(int));
-int c=l;
-    int p1,p2;
-    p1 = l;p2=((l+r)/2)+1;
-    while ((p1<((l+r)/2)+1) &&(p2<r+1))
-    { if(a[p1] <= a[p2])
-{          b[c++] = a[p1];
-           p1++;
-}
-          else
-          {b[c++] = a[p2];p2++;}
-        
-    };
-    
-    if (p2 == r+1)
-    {
-       while ((p1<((l+r)/2)+1))
-    { 
-          b[c++] = a[p1];
-            p1++;
-    }; 
-        
-    }
-    else
-    {
-          while ((p2<r+1))
-    { 
-          b[c++] = a[p2];
-p2++;
-    }; 
-         }
-         
+//To merge //
+void merge(int a[],int l,int r,int n)
+{  
+	int *b = (int*)malloc(n*sizeof(int));
+	int c=l;
+    	int p1,p2;
+    	p1 = l;
+	p2=((l+r)/2)+1;
+	
+	//COMPARING AND APPENDING THE ELEMENTS IN ACSENDING ORDER//
+    	while ((p1<((l+r)/2)+1) && (p2<r+1))
+    	{ 
+		if(a[p1] <= a[p2])
+		{          
+			b[c++] = a[p1];
+           		p1++;
+		}
+          	else
+         	{
+			b[c++] = a[p2];
+			p2++;
+		}
+       	}
+    	//IF ANY OF THE ARRAY HAS ANY EXTRA ELEMENT(S) LEFT ITS IS APPENDED TO b AS IT IS//
+    	if (p2 == r+1)
+	{
+	       while (p1 < ((l + r) / 2) + 1)
+	       {
+		       b[c++] = a[p1];
+		       p1++;
+    		} 
+    	}
+    	else
+    	{
+          	while ((p2<r+1))
+    		{ 
+          		b[c++] = a[p2];
+			p2++;
+		}
+	}
+	//THE ORIGINAL ARRAY IS UPDATED BACK WITH SORTED ELEMENTS//
         for (c=l;c<r-l+1;c++)
         a[c] = b[c];
     
@@ -54,36 +61,38 @@ void mergesort(int a[],int n,int l,int r)
         swap(&a[l],&a[r]);
         
     }
-    else if(l==r)
-    {}
     else
-    {mergesort(a,n,l,(l+r)/2);
-     mergesort(a,n,((l+r)/2)+1,r);
-       merge(a,l,r,n);
-        
+    {
+	    //DISMANTLING THE WHOLE ARRAY//
+	    mergesort(a,n,l,(l+r)/2);
+	    mergesort(a,n,((l+r)/2)+1,r);
+	    //ASSEMBLING THE DISMANTLED ARRAY//
+	    merge(a,l,r,n);
     }
     
 }
 int main(void) {   //main function//
-int *a,n,i;
-scanf("%d",&n);
-a = (int*)malloc(n*sizeof(int));
-for (i=0;i<n;i++)
-{
-    
-    scanf("%d",&a[i]);
-    
-}
 
-mergesort(a,n,0,n-1);
-for (i=0;i<n;i++)
-{
-    
-    printf(" %d",&a[i]);
-    
-}
-
-
+	int *a,n,i;
+	scanf("%d",&n);
+	
+	//CREATING A DYNAMIC ARRAY OF INTIAL SIZE 'n'//
+	a = (int*)malloc(n*sizeof(int));
+	
+	//INPUT OF ARRAY ELEMENTS//
+	for (i=0;i<n;i++)
+	{
+    		scanf("%d",&a[i]);
+	}
+	
+	//CALLING THE SORTING FUNCTION//
+	mergesort(a,n,0,n-1);
+	
+	//VISUALIZATION OF ARRAY AFTER IT IS SORTED//
+	for (i=0;i<n;i++)
+	{
+		printf(" %d",&a[i]);  
+	}
 	return 0;
 }
 
